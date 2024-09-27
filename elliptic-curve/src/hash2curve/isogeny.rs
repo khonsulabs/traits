4801cca75a3947725489549c11c6356074a73497
Artyom Pavlov <newpavlov@gmail.com>
2024-09-27 18:33:07 +0300

    elliptic-curve: add missing `Debug` impls (#1675)

--- elliptic-curve/src/hash2curve/isogeny.rs
+++ elliptic-curve/src/hash2curve/isogeny.rs
@@ -7,6 +7,7 @@ use ff::Field;
 use generic_array::{typenum::Unsigned, ArrayLength, GenericArray};
 
 /// The coefficients for mapping from one isogenous curve to another
+#[derive(Debug)]
 pub struct IsogenyCoefficients<F: Field + AddAssign + Mul<Output = F>> {
     /// The coefficients for the x numerator
     pub xnum: &'static [F],
